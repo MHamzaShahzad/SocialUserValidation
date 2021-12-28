f357d5bebe429f3361e60e66f39363445a4120a9
MHamzaShahzad <hamzashahzad604@gmail.com>
2021-12-28 11:39:49 +0500

    updated fro netlify-lambda

--- functions/validations.js
+++ functions/validations.js
@@ -1,4 +1,35 @@
+const appleSignin = require('apple-signin-auth');
+
 exports.handler = async event => {
+    try {
+        const { body } = req;
+        const {
+            email,
+            fullName,
+            identityToken
+        } = body;
+
+        //const clientId = <your_app_id>;
+        // verify token (will throw error if failure)
+        const { sub: userAppleId } = await
+            appleSignin.verifyIdToken(identityToken, {
+                audience: clientId,
+                ignoreExpiration: true, // ignore token expiry (never expires)
+            });
+
+        if (appleUserId === req.body.appleUserId) {
+            //details match, continue with sign in process
+            res.json({
+                ack: 'error',
+                message: 'failed to verify identityToken'
+            });
+        }
+    } catch (e) {
+        res.json({
+            ack: 'error',
+            message: 'failed to verify identityToken'
+        });
+    }
     return {
         statusCode: 200,
         body: "Hello World"
